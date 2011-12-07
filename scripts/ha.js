6230dce830dfc9a704be442ec32c697f3ea6d01e
Jean-Hugues Pinson <jh.pinson@oxys.net>
2011-12-07 16:14:34 +0100

    scripts

--- scripts/ha.js
+++ scripts/ha.js
@@ -3,29 +3,23 @@ var Host = require('../lib/host').Host;
 var CzAgendaApi = require('../lib/containers/czagendaapi').Container;
 var CzagendaHttpProxy = require('../lib/containers/czagendahttpproxy').Container;
 var Container = require('../lib/container').Container;
-var async = require('async');
+
 var ContainerTypes = require('../lib/containerTypes');
 var HostManager = require('../lib/hostsManager');
+var HASupervisor = require('../lib/haSupervisor');
 
 ContainerTypes.register(CzAgendaApi);
 
-
-
 var hm = new HostManager(new Host('10.7.35.110'));
 hm.register(new Host('10.7.35.110'), function() {
-	
-	hm.getContainers({
-						type : CzagendaHttpProxy.type
-					}, function(err, ctn) {
-						console.log(ctn)
-					})
-	
-			/*hm.getContainer({
-						id : 120
-					}, function(err, ctn) {
-						ctn.isAlive(function(err, alive) {
-									console.log(alive);
-								});
-					})*/
 
+			var has = new HASupervisor(hm, {
+						type : CzAgendaApi.type,
+						masterIP : '10.7.51.2'
+					});
+		
+			has.start(function () {
+				console.log(arguments)
+			})	
+				
 		});
