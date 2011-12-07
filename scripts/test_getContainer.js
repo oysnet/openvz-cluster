cbe850c7cb5cd5c07b63caf3d1b654f08abf0770
Jean-Hugues Pinson <jh.pinson@oxys.net>
2011-12-07 10:04:00 +0100

    scripts

--- scripts/test_getContainer.js
+++ scripts/test_getContainer.js
@@ -6,12 +6,8 @@ var async = require('async');
 
 var h = new Host('10.7.35.110');
 
-h.registerType(Gitolite);
-
-h.getContainer({id:130}, function (err, ctn) {
-	ctn.addUser('hugo', 'ssh-dss AAAAB3NzaC1kc3MAAACBAPmfPYN65/Def123ufh5cXs8L7hr2l1rFQjfaPIjxKG9lgQwrHwJEg1AHnA0HEkKgCkPpDHQzbLWUAKVRV+FekfJe7SaLajMSGXbO2bVo+KCT4AiHUE650CAE3VJBJ60fnqFO0xJpUrpaR/p0dOmOY4opJKLUg3suP0buuFOEU9VAAAAFQC2ZqVdudxBXUMeLQ+str1Zva5FPQAAAIBbgjF+ORWiHiUGB1SPBRh3o3NcuB7LLa1EGpq+o1q/dd6haVnIaNj59j6yBM3Gx1uhkNmA7zy8VD0Hhph4ZBROtgvwtDy0QLG9LDDe3LQjEZE/foN4f0MykK+ZUBcZjv2nUarOFq28lJ9Ap1/u7wTDzibOuVwqJDtth/HC4DZ+GQAAAIEAiLnM6AURbg8qEhGMpo3EfUzkITvKrzZtqYbNfErVERww706Shan2yWxVddBVvCWTXmvhkpKsf6Q2BEatrWgcHKgDNBsClvlFsVSLGmG5BKzYpcZAO0ka+JnQSrVt/zg8sZeX8+ohyi2osNIeY0QYDXQp9/TE0BmvVLmMSdbhX/o= hugo@anywhere',
-		function () {
-			ctn.addUserInGroup('hugo', 'admins')
-		}
-	)
+h.getContainer({id:120}, function (err, ctn) {
+	ctn.isAlive(function (err, alive) {
+		console.log(alive);
+	});
 })
