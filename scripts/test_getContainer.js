0784a05727e29265649a1f273dd6fa8475d4ef1d
Jean-Hugues Pinson <jh.pinson@oxys.net>
2011-12-07 08:49:41 +0100

    scripts

--- scripts/test_getContainer.js
+++ scripts/test_getContainer.js
@@ -1,15 +1,17 @@
 var Host = require('../lib/host').Host;
 
-var Czagendaapi = require('../lib/containers/czagendaapi').Container;
+var Gitolite = require('../lib/containers/gitolite').Container;
 var Container = require('../lib/container').Container;
 var async = require('async');
 
 var h = new Host('10.7.35.110');
 
-h.registerType(Czagendaapi);
+h.registerType(Gitolite);
 
-h.getContainer({id:136}, function (err, ctn) {
-	ctn.start(function () {
-		console.log(arguments)
-	})
+h.getContainer({id:130}, function (err, ctn) {
+	ctn.addUser('hugo', 'ssh-dss AAAAB3NzaC1kc3MAAACBAPmfPYN65/Def123ufh5cXs8L7hr2l1rFQjfaPIjxKG9lgQwrHwJEg1AHnA0HEkKgCkPpDHQzbLWUAKVRV+FekfJe7SaLajMSGXbO2bVo+KCT4AiHUE650CAE3VJBJ60fnqFO0xJpUrpaR/p0dOmOY4opJKLUg3suP0buuFOEU9VAAAAFQC2ZqVdudxBXUMeLQ+str1Zva5FPQAAAIBbgjF+ORWiHiUGB1SPBRh3o3NcuB7LLa1EGpq+o1q/dd6haVnIaNj59j6yBM3Gx1uhkNmA7zy8VD0Hhph4ZBROtgvwtDy0QLG9LDDe3LQjEZE/foN4f0MykK+ZUBcZjv2nUarOFq28lJ9Ap1/u7wTDzibOuVwqJDtth/HC4DZ+GQAAAIEAiLnM6AURbg8qEhGMpo3EfUzkITvKrzZtqYbNfErVERww706Shan2yWxVddBVvCWTXmvhkpKsf6Q2BEatrWgcHKgDNBsClvlFsVSLGmG5BKzYpcZAO0ka+JnQSrVt/zg8sZeX8+ohyi2osNIeY0QYDXQp9/TE0BmvVLmMSdbhX/o= hugo@anywhere',
+		function () {
+			ctn.addUserInGroup('hugo', 'admins')
+		}
+	)
 })
