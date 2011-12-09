0f6af40143baac8131506de499757af8d6c1c8e4
Jean-Hugues Pinson <jh.pinson@oxys.net>
2011-12-09 09:06:02 +0100

    scripts

--- scripts/ha.js
+++ scripts/ha.js
@@ -1,19 +1,20 @@
 var Host = require('../lib/host').Host,
     Cluster = require('../lib/cluster').Cluster,
     Redis =  require('../lib/containers/redis').Container,
+    TestContainer =  require('./testContainer').Container,
     HA = require('../lib/supervisors/ha').HA;
     ContainerTypes = require('../lib/containerTypes'),
     Count = require('../lib/supervisors/count').Count;
     
-ContainerTypes.register(Redis);
+ContainerTypes.register(TestContainer);
 
 var h = new Host('10.7.35.110');
 var cluster = new Cluster()
 cluster.register(h);
 
 cluster.afterInit(function() {
-  //new HA(cluster,Redis,'10.7.35.177');
-  new Count(cluster,Redis,3);
+  //new HA(cluster,TestContainer,'10.7.35.178');
+  new Count(cluster,TestContainer,1);
   
 });
  
