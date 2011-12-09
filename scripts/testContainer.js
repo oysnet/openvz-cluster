2ff88719fad9b809b6c376db5695f66660e8da2c
Jean-Hugues Pinson <jh.pinson@oxys.net>
2011-12-09 09:06:16 +0100

    testcontainer

---
+++ scripts/testContainer.js
@@ -0,0 +1,25 @@
+var util = require('util'),
+    log = require('czagenda-log').from(__filename),
+    Container = require('../lib/container').Container;
+    
+
+var TestContainer = function TestContainer() {
+  Container.apply(this,Array.prototype.slice.call(arguments));
+  
+};
+util.inherits(TestContainer, Container);
+
+TestContainer.prototype.setup = function(callback) {
+  log.info('Setup new TestContainer');
+  Container.prototype.setup.apply(this,[function(err,res) {
+      if(err) {
+        callback(err);
+        return;
+      }
+      this.saveDescription(callback);
+  }.bind(this)]);
+}
+
+TestContainer.type = 'TestContainer';
+
+exports.Container = TestContainer
