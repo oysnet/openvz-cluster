1ee3cf1bcc7874732361f2235319af809022dfaa
Jean-Hugues Pinson <jh.pinson@oxys.net>
2011-12-16 17:24:52 +0100

    scripts

--- scripts/ha.js
+++ scripts/ha.js
@@ -2,18 +2,33 @@ var Host = require('../lib/host').Host, Cluster = require('../lib/cluster').Clus
 ContainerTypes = require('../lib/containerTypes'), Count = require('../lib/supervisors/count').Count;
 
 var CzagendaHttpProxy = require('../lib/containers/czagendahttpproxy').Container;
+var CzagendaApi = require('../lib/containers/czagendaapi').Container
+var CzagendaElasticSearch = require('../lib/containers/czagendaelasticsearch').Container
+var CzagendaRedis = require('../lib/containers/czagendaredis').Container
 
 ContainerTypes.register(CzagendaHttpProxy);
+ContainerTypes.register(CzagendaApi);
+ContainerTypes.register(CzagendaElasticSearch);
+ContainerTypes.register(CzagendaRedis);
 
 var h = new Host('10.7.35.110');
 
+var cluster = new Cluster();
+cluster.register(h);
 
-
-  var cluster = new Cluster();
-  cluster.register(h);
-  
-  cluster.afterInit(function() { 
-  new HA(cluster,CzagendaHttpProxy,'10.7.100.1');
-  new Count(cluster,CzagendaHttpProxy,2, false);
-  
-  });
+cluster.afterInit(function() {
+			// HTTP PROXY
+			new HA(cluster, CzagendaHttpProxy, '10.7.100.1');
+			new Count(cluster, CzagendaHttpProxy, 2, false);
+			
+			// API
+			new Count(cluster, CzagendaApi, 2);
+			
+			// ElasticSearch
+			new HA(cluster, CzagendaElasticSearch, '10.7.100.2');
+			new Count(cluster, CzagendaElasticSearch, 3)
+			
+			// Redis
+			new HA(cluster, CzagendaRedis, '10.7.100.3');
+			new Count(cluster, CzagendaRedis, 1)
+		});
--- scripts/test_getContainer.js
+++ scripts/test_getContainer.js
@@ -7,9 +7,9 @@ var async = require('async');
 var ContainerTypes = require('../lib/containerTypes');
 var Cluster = require('../lib/cluster').Cluster;
 
-ContainerTypes.register(CzagendaHttpProxy);
+ContainerTypes.register(CzAgendaApi);
 
-
+console.log('ok')
 var h = new Host('10.7.35.110');
 
 
@@ -18,28 +18,10 @@ var h = new Host('10.7.35.110');
   cluster.register(h);
   
   cluster.afterInit(function() { 
-  	var container = cluster.getContainerById(217);
-		
-  	container.on('changeStatus',function (status) {
-  		console.log('changeStatus', status)
-  	})
-  	
-  	container.on('changeVmStatus',function (status) {
-  		console.log('changeVmStatus', status)
-  	})
-  	
-  	console.log('Container 217', container.status, container.vmStatus)
-  	
-  	setTimeout(function () {
-  		console.log('set vmStatus 5')
-  		container.refreshInfo({vmStatus : 5});
-  		
-  		setTimeout(function () {
-  		console.log('set vmStatus 1')
-  		container.refreshInfo({vmStatus : 1});
-  	}, 5000)
-  		
-  	}, 5000)
+  	console.log('init')
+  	var container = cluster.getContainerById(219);
+		container.setRam(2048, console.log)
+  	  
   	
   });
 
