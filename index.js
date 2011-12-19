6376a301de3ccecd8d6bd97d8cf351e063a49c55
Jean-Hugues Pinson <jh.pinson@oxys.net>
2011-12-19 08:22:51 +0100

    package app

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 exports.Host = require('./lib/host').Host;
-exports.Cluster = require('./lib/host').Cluster;
+exports.Cluster = require('./lib/cluster').Cluster;
 exports.Container = require('./lib/container').Container;
+exports.containerTypes = require('./lib/containerTypes');
+
+exports.containers = require('./lib/containers');
+exports.supervisors = require('./lib/supervisors');
